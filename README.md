d0083ef45bc954c5c6030bfa4ca7358359897918
Yves Gallot <59421144+galloty@users.noreply.github.com>
2022-02-20 13:44:06 +0100

    Slightly faster

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ Search for quadruples (*k* 2<sup>*n* - 1</sup> - 1, *k* 2<sup>*n*</sup> - 1, *k*
 We must have *k* = 15 (mod 30). If the sieve range is *k*<sub>*min*</sub> = 3, *k*<sub>*max*</sub> = 2<sup>32</sup>, *n*<sub>*min*</sub> = 3321925 and *n*<sub>*max*</sub> = *n*<sub>*min*</sub> + 63 then the bitmap size is 1 gigabyte.
 
 *qsieve* is multithreaded:
-- Thread 1 is a prime number generator based on a segmented sieve of Eratosthenes and a strong probable prime to base 2.
+- Thread 1 is a pseudo prime number generator based on a segmented sieve of Eratosthenes.
 - Thread 2 computes 1 / 15 · 2<sup>*n*_*min* - 1</sup> mod *p* for each *p*.
 - Thread 3 fills the bitmap for all *k* and *n* in the range such that *k* 2<sup>*n*</sup> &plusmn; 1 = 0 (mod *p*).
 
 The expected number of remaining candidates is 0.41252 · Δ*k* · Δ*n* / (log *p*<sub>*max*</sub>)<sup>4</sup>.<br>
-For 3 &le; *k* < 2<sup>32</sup>, 3321925 &le; *n* &le; 3321988 and *p*<sub>*max*</sub> = 10<sup>10</sup>, 403387 candidates are expected and their actual number is 404448.
+For 3 &le; *k* < 2<sup>32</sup>, 3321925 &le; *n* &le; 3321988 and *p*<sub>*max*</sub> = 10<sup>12</sup>, 194534 candidates are expected and their actual number is 195115.
 
 ## Build
 
--- src/qsieve.cpp
+++ src/qsieve.cpp
@@ -18,114 +18,117 @@ Please give feedback to the authors if improvement is realized. It is distribute
 #include <thread>
 #include <mutex>
 
-static uint64_t neg_mod(const uint64_t x, const uint64_t p) { return (x == 0) ? 0 : p - x; }
+constexpr static uint64_t neg_mod(const uint64_t x, const uint64_t p) { return (x == 0) ? 0 : p - x; }
 
-static uint64_t add_mod(const uint64_t x, const uint64_t y, const uint64_t p)
+constexpr static uint64_t add_mod(const uint64_t x, const uint64_t y, const uint64_t p)
 {
 	const uint64_t c = (x >= p - y) ? p : 0;
 	return x + y - c;
 }
 
-static uint64_t sub_mod(const uint64_t x, const uint64_t y, const uint64_t p)
+constexpr static uint64_t sub_mod(const uint64_t x, const uint64_t y, const uint64_t p)
 {
 	const uint64_t c = (x < y) ? p : 0;
 	return x - y + c;
 }
 
-static uint64_t half_mod(const uint64_t x, const uint64_t p)
+constexpr static uint64_t half_mod(const uint64_t x, const uint64_t p)
 {
 	const uint64_t c = (x % 2 != 0) ? p : 0;
 	return (x + c) / 2;
 }
 
-class mod
+// 3 < p < 2^63
+constexpr static uint64_t oneThird_mod(const uint64_t p)
+{
+	return (p % 3 == 2) ? (p + 1) / 3 : (2 * p + 1) / 3;
+}
+
+// 5 < p < 2^64 / 3
+constexpr static uint64_t oneFifth_mod(const uint64_t p)
+{
+	const uint64_t r = p % 5;
+	if (r == 1) return (4 * p + 1) / 5;
+	if (r == 2) return (2 * p + 1) / 5;
+	if (r == 3) return (3 * p + 1) / 5;
+	return (p + 1) / 5;
+}
+
+class res4
 {
 private:
-	const uint64_t _p;
-	const int _shift;
-	const uint64_t _q;
+	uint64_t _r[4];
+
+public:
+	uint64_t operator [](const size_t i) const { return _r[i]; }
+	uint64_t & operator [](const size_t i) { return _r[i]; }
+};
+
+class mod4
+{
+private:
+	res4 _p;
+	int _shift[4];
+	uint64_t _q[4];
 
 private:
 	constexpr static int log2_64(const uint64_t x) { return 63 - __builtin_clzll(x); }
 	constexpr static uint64_t mulhi_64(const uint64_t x, const uint64_t y) { return uint64_t((x * __uint128_t(y)) >> 64); }
 
-public:
-	mod(const uint64_t p) : _p(p), _shift(log2_64(p) - 1), _q(uint64_t((__uint128_t(1) << (64 + _shift)) / p)) {}
-
-	// 2 < p
-	constexpr uint64_t oneHalf() const { return (_p + 1) / 2; }
-
-	// 3 < p < 2^63
-	constexpr uint64_t oneThird() const
+	res4 half(const res4 & x) const
 	{
-		return (_p % 3 == 2) ? (_p + 1) / 3 : (2 * _p + 1) / 3;
+		res4 r;
+		for (size_t i = 0; i < 4; ++i) r[i] = half_mod(x[i], _p[i]);
+		return r;
 	}
-
-	// 5 < p < 2^64 / 3
-	constexpr uint64_t oneFifth() const
-	{
-		const uint64_t r = _p % 5;
-		if (r == 1) return (4 * _p + 1) / 5;
-		if (r == 2) return (2 * _p + 1) / 5;
-		if (r == 3) return (3 * _p + 1) / 5;
-		return (_p + 1) / 5;
-	}
-
-	uint64_t neg(const uint64_t x) const { return neg_mod(x, _p); }
-	uint64_t add(const uint64_t x, const uint64_t y) const { return add_mod(x, y, _p); }
-	uint64_t sub(const uint64_t x, const uint64_t y) const { return sub_mod(x, y, _p); }
-	uint64_t half(const uint64_t x) const { return half_mod(x, _p); }
 
 	// Barrett's product: let n = 63, r = ceil(log2(p)) [2^{r - 1} < p < 2^r], s = r - 2 = floor(log2(p)) - 1,
 	// t = n + 1 = 64, q = floor(2^{s + t} / p). Then the number of iterations h = 1.
 	// We must have x.y < alpha.p with alpha = 2^{n-2}. If p <= 2^{n-2} = 2^61 then x^2 < p^2 <= alpha.p.
-	uint64_t mul(const uint64_t x, const uint64_t y) const
+	uint64_t mul(const uint64_t x, const uint64_t y, const size_t i) const
 	{
 		const __uint128_t xy = x * __uint128_t(y);		// 0 <= ab < 2^{2r}
-		const uint64_t q_p = uint64_t(xy >> _shift);	// 0 <= q_p < 2^{r + 2} <= 2^63
-		const uint64_t r = uint64_t(xy - mulhi_64(q_p, _q) * __uint128_t(_p));
-		return (r >= _p) ? r - _p : r;
+		const uint64_t q_p = uint64_t(xy >> _shift[i]);	// 0 <= q_p < 2^{r + 2} <= 2^63
+		const uint64_t r = uint64_t(xy - mulhi_64(q_p, _q[i]) * __uint128_t(_p[i]));
+		return (r >= _p[i]) ? r - _p[i] : r;
 	}
 
-	uint64_t oneHalfPow(const uint64_t e) const
+public:
+	mod4(const res4 & p)
 	{
-		uint64_t r = oneHalf();
-		for (int b = log2_64(e) - 1; b >= 0; --b)
+		for (size_t i = 0; i < 4; ++i)
+		{
+			_p[i] = p[i];
+			_shift[i] = log2_64(p[i]) - 1;
+			_q[i] = uint64_t((__uint128_t(1) << (64 + _shift[i])) / p[i]);
+		}
+	}
+
+	res4 mul(const res4 & x, const res4 & y) const
+	{
+		res4 r;
+		for (size_t i = 0; i < 4; ++i) r[i] = mul(x[i], y[i], i);
+		return r;
+	}
+
+	res4 oneFifteenth() const
+	{
+		res4 r;
+		for (size_t i = 0; i < 4; ++i) r[i] = mul(oneThird_mod(_p[i]), oneFifth_mod(_p[i]), i);
+		return r;
+	}
+
+	res4 oneHalfPow(const uint64_t e) const
+	{
+		res4 r;
+		for (size_t i = 0; i < 4; ++i) r[i] = (_p[i] + 1) / 2;
+		int b = log2_64(e) - 1;
+		do
 		{
 			r = mul(r, r);
 			if ((e & (uint64_t(1) << b)) != 0) r = half(r);
-		}
+		} while (--b >= 0);
 		return r;
-	}
-
-	uint64_t twoPow(const uint64_t e) const
-	{
-		uint64_t r = 2;
-		for (int b = log2_64(e) - 1; b >= 0; --b)
-		{
-			r = mul(r, r);
-			if ((e & (uint64_t(1) << b)) != 0) r = add(r, r);
-		}
-		return r;
-	}
-
-	bool spsp2() const
-	{
-		// n - 1 = 2^k * r
-		uint64_t r = _p - 1;
-		int k = 0; for (; r % 2 == 0; r /= 2) ++k;
-
-		uint64_t x = twoPow(r);
-		if (x == 1) return true;
-
-		// Compute x^(2^i) for 0 <= i < n.  If any are -1, n is a a-spsp.
-		for (; k > 0; --k)
-		{
-			if (x == _p - 1) return true;
-			x = mul(x, x);
-		}
-
-		return false;
 	}
 };
 
@@ -141,8 +144,8 @@ public:
 		_bmp.resize(k_size * n_size, false);
 	}
 
-	bool get(const size_t k, const size_t n) const { return _bmp[n * _k_size + k]; }
-	void set(const size_t k, const size_t n) { if (!_bmp[n * _k_size + k]) _bmp[n * _k_size + k] = true; }
+	bool get(const size_t k, const size_t n) const { const size_t i = n * _k_size + k; return _bmp[i]; }
+	void set(const size_t k, const size_t n) { const size_t i = n * _k_size + k; if (!_bmp[i]) _bmp[i] = true; }
 
 	size_t size() const { return _bmp.size(); }
 
@@ -190,54 +193,50 @@ private:
 private:
 	void gen_p()
 	{
-		// Segmented sieve of Eratosthenes: outputs have no factor < 65537.
-		static const uint32_t sp_max = 1 << 16;
+		// Segmented sieve of Eratosthenes: outputs have no factor < 2^20. 2^40 ~ 10^12
+		static const uint32_t sp_max = 1 << 20;
 		static const size_t sieve_size = sp_max / 2;	// sieve with an odd prime table.
-		static const size_t odd_prime_count = 6541;		// # odd primes with p < 2^16.
 
-		bool sieve[sieve_size];
-		uint32_t prm[odd_prime_count];
-		uint32_t prm_ptr[odd_prime_count];
+		std::vector<bool> sieve(sieve_size, false);
+		std::vector<uint32_t> v_prm, v_prm_ptr;
 
-		prm[0] = 3; prm[1] = 5; prm[2] = 7;
-		uint32_t i = 3;
+		v_prm.push_back(3); v_prm.push_back(5); v_prm.push_back(7);
 		for (uint32_t k = 11; k < sp_max; k += 2)
 		{
 			const uint32_t s = uint32_t(std::sqrt(double(k))) + 1;
 			uint32_t d; for (d = 3; d <= s; d += 2) if (k % d == 0) break;
-			if (d > s) prm[i++] = k;
+			if (d > s) v_prm.push_back(k);
 		}
 
-		// if (i != odd_prime_count) throw;
-
-		for (size_t k = 0; k < sieve_size; ++k) sieve[k] = false;
-
 		const uint64_t p0 = (_p_min / sp_max) * sp_max, p1 = (_p_max / sp_max + 1) * sp_max;
-		std::cout << "p in [" << p0 << "; " << p1 << "] " << std::endl;
+		std::cout << "Sieve of Eratosthenes: " << v_prm.size() << " primes (" << v_prm.size() * 8 / 1024 << " kB), bitmap size: " << sieve_size / (8 * 1024)
+				  << " kB, p in [" << std::max(p0, uint64_t(7)) << "; " << p1 << "] " << std::endl;
 
 		if (p0 == 0)
 		{
 			sieve[0] = true;	// p = 1
-			for (size_t i = 0; i < odd_prime_count; ++i)
+			for (const size_t p : v_prm)
 			{
-				const size_t p = prm[i];
 				bool first = true;
 				size_t k = (p - 1) / 2;
 				for (; k < sieve_size; k += p) if (first) first = false; else sieve[k] = true;
-				prm_ptr[i] = uint32_t(k - sieve_size);
+				v_prm_ptr.push_back(uint32_t(k - sieve_size));
 			}
 		}
 		else
 		{
-			for (size_t i = 0; i < odd_prime_count; ++i)
+			for (const size_t p : v_prm)
 			{
-				const size_t p = prm[i];
 				size_t o = p - size_t(p0 % p); if (o % 2 == 0) o += p;
 				size_t k = (o - 1) / 2;
 				for (; k < sieve_size; k += p) sieve[k] = true;
-				prm_ptr[i] = uint32_t(k - sieve_size);
+				v_prm_ptr.push_back(uint32_t(k - sieve_size));
 			}
 		}
+
+		const size_t odd_prime_count = v_prm.size();
+		const uint32_t * const prm = v_prm.data();
+		uint32_t * const prm_ptr = v_prm_ptr.data();
 
 		PArray p_array;
 		size_t p_array_i = 0;
@@ -250,7 +249,7 @@ private:
 				if (!sieve[kp])
 				{
 					const uint64_t p = jp + 2 * kp + 1;
-					if ((p > 5) && ((p < 65537 * uint64_t(65537)) || mod(p).spsp2()))
+					if (p > 5)
 					{
 						p_array.p[p_array_i] = p;
 						p_array_i = (p_array_i + 1) % p_size;
@@ -269,7 +268,7 @@ private:
 				}
 			}
 
-			for (size_t k = 0; k < sieve_size; ++k) sieve[k] = false;
+			sieve.assign(sieve_size, false);
 
 			for (size_t i = 0; i < odd_prime_count; ++i)
 			{
@@ -290,7 +289,6 @@ private:
 	void gen_r()
 	{
 		PArray p_array;
-		PRArray pr_array;
 		size_t queue_size = 0;
 
 		while (true)
@@ -313,13 +311,20 @@ private:
 			}
 			else
 			{
-				for (size_t i = 0; i < p_size; ++i)
+				PRArray pr_array;
+
+				for (size_t i = 0; i < p_size; i += 4)
 				{
-					const uint64_t p = p_array.p[i];
-					const mod m = mod(p);
-					PR & pr = pr_array.pr[i];
-					pr.p = p;
-					pr.r = m.mul(m.oneHalfPow(_n_min - 1), m.mul(m.oneThird(), m.oneFifth()));	// r = 1/2^(n_min - 1) / 15 (mod p)
+					res4 p; for (size_t j = 0; j < 4; ++j) p[j] = p_array.p[i + j];
+					const mod4 m = mod4(p);
+
+					// r = 1/2^(n_min - 1) / 15 (mod p)
+					const res4 r = m.mul(m.oneHalfPow(_n_min - 1), m.oneFifteenth());
+					for (size_t j = 0; j < 4; ++j)
+					{
+						PR & pr = pr_array.pr[i + j];
+						pr.p = p[j]; pr.r = r[j];
+					}
 				}
 
 				std::lock_guard<std::mutex> guard(_pr_queue_mutex);
@@ -408,7 +413,7 @@ public:
 
 		bitmap bmap(_k_range, _n_range);
 
-		std::cout << bmap.size() / (8 << 20) << " MB" << std::endl;
+		std::cout << "Bitmap size: " << bmap.size() / (8 << 20) << " MB" << std::endl;
 
 		PRArray pr_array;
 		uint64_t last_p = 0;
@@ -445,7 +450,7 @@ public:
 
 				const auto t1 = std::chrono::steady_clock::now();
 				const std::chrono::duration<double> dt = t1 - t0;
-				if ((dt.count() > 5 * 60) || (duration == 0))
+				if ((dt.count() > 15 * 60) || (duration == 0))
 				{
 					t0 = t1; duration += dt.count();
 					const size_t expected = size_t(0.41252 * (k_max - k_min + 1) * _n_range / std::pow(std::log(double(last_p)), 4));
@@ -461,7 +466,7 @@ public:
 		std::cout << std::scientific << std::setprecision(2) << double(p_max) << ": ";
 		std::cout << bmap.count() << " candidates, " << expected << " expected, " << std::lrint(duration) << " sec." << std::endl;
 
-		for (size_t k = 0; k < _k_range; ++k)
+/*		for (size_t k = 0; k < _k_range; ++k)
 		{
 			for (size_t n = 0; n < _n_range; ++n)
 			{
@@ -471,7 +476,7 @@ public:
 				}
 			}
 		}
-	}
+*/	}
 
 	virtual ~qsieve() {}
 };
@@ -483,7 +488,9 @@ int main(int argc, char * argv[])
 	std::cerr << " qsieve is free source code, under the MIT license." << std::endl << std::endl;
 	std::cerr << " Usage: qsieve <p_max> <k_min> <k_max>" << std::endl << std::endl;
 
-	uint64_t p_max = (argc > 1) ? std::atoll(argv[1]) : uint64_t(-1) / 4;
+	// uint64_t p_max = (argc > 1) ? std::atoll(argv[1]) : uint64_t(-1) / 4;
+	uint64_t T = 1; for (size_t i = 0; i < 12; ++i) T *= 10;
+	uint64_t p_max = (argc > 1) ? std::atoll(argv[1]) : T;
 	if (p_max < 7) p_max = 7;
 	if (p_max > uint64_t(-1) / 4) p_max = uint64_t(-1) / 4;
 
@@ -491,6 +498,7 @@ int main(int argc, char * argv[])
 	k_min /= 30; k_min *= 30; k_min += 15;
 
 	uint64_t k_max = (argc > 3) ? std::atoll(argv[3]) : k_min + 30 * uint64_t(143165576);
+	// uint64_t k_max = (argc > 3) ? std::atoll(argv[3]) : k_min + 30 * uint64_t(100000);
 	k_max /= 30; k_max *= 30; k_max += 15;
 	if (k_max < k_min) k_max = k_min;
 
